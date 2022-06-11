c5dff8daf71cb4600fb285c4e7611ac5d7175611
Eli Sachs <easachs13@gmail.com>
2022-06-11 12:08:58 -0600

    Add CLI to Encrypt runner

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,12 +1,16 @@
 require_relative 'enigma'
 
-handle = File.open(ARGV[0], 'r')
-message = handle.read
-handle.close
+unencrypted_src = File.join('.', 'msgs', 'unencrypted', ARGV[0])
+unencrypted = File.open(unencrypted_src, 'r')
+message = unencrypted.read
+unencrypted.close
 
 enigma = Enigma.new
 encrypted = enigma.encrypt(message)
 
-writer = File.open([ARGV[1], 'w')
-writer.write(encrypted)
+writer_src = File.join('.', 'msgs', 'encrypted', ARGV[1])
+writer = File.open(writer_src, 'w')
+writer.write(encrypted[:encryption])
 writer.close
+
+puts "Created '#{ARGV[0]}' with the key #{encrypted[:key]} and date #{encrypted[:date]}"
