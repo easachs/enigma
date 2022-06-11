52bd4e0582771af6222e28a2ce20dace7b3d41c4
Eli Sachs <easachs13@gmail.com>
2022-06-11 11:23:33 -0600

    Create Encrypt runner

---
+++ lib/encrypt.rb
@@ -0,0 +1,12 @@
+require_relative 'enigma'
+
+handle = File.open(ARGV[0], 'r')
+message = handle.read
+handle.close
+
+enigma = Enigma.new
+encrypted = enigma.encrypt(message)
+
+writer = File.open([ARGV[1], 'w')
+writer.write(encrypted)
+writer.close
