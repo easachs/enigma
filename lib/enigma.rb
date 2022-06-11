2d1d4230e8fa0ab16b3f741619e32a6a01c50b98
Eli Sachs <easachs13@gmail.com>
2022-06-11 17:03:47 -0600

    Add Enigma attributes/tests

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,7 +2,7 @@ require_relative 'encryptor'
 require_relative 'decryptor'
 require 'date'
 class Enigma
-
+  attr_reader :today, :autokey
   def initialize
     @today = Date.today.strftime('%d%m%y')
     @autokey = '%05d' % rand(10**5)
