99e0a9cc7a35829170c96cba3a2e0c065be431c3
Eli Sachs <easachs13@gmail.com>
2022-06-11 13:54:27 -0600

    Rewrite Encrypt/Decrypt runners for feature

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,18 +1,4 @@
 require_relative 'enigma'
 
-encrypted_src = File.join('.', 'msgs', 'encrypted', ARGV[0])
-encrypted = File.open(encrypted_src, 'r')
-cipher = encrypted.read
-encrypted.close
-
-enigma = Enigma.new
-unencrypted = enigma.decrypt(cipher, ARGV[2], ARGV[3])
-
-writer_src = File.join('.', 'msgs', 'unencrypted', ARGV[1])
-writer = File.open(writer_src, 'w')
-writer.write(unencrypted[:decryption])
-writer.close
-
-puts "Created '#{ARGV[1]}' with the key #{unencrypted[:key]} and date #{unencrypted[:date]}"
-
+Enigma.decrypt(ARGV[0], ARGV[1], ARGV[2], ARGV[3])
 # 38901, 110622
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,16 +1,3 @@
 require_relative 'enigma'
 
-unencrypted_src = File.join('.', 'msgs', 'unencrypted', ARGV[0])
-unencrypted = File.open(unencrypted_src, 'r')
-message = unencrypted.read
-unencrypted.close
-
-enigma = Enigma.new
-encrypted = enigma.encrypt(message)
-
-writer_src = File.join('.', 'msgs', 'encrypted', ARGV[1])
-writer = File.open(writer_src, 'w')
-writer.write(encrypted[:encryption])
-writer.close
-
-puts "Created '#{ARGV[1]}' with the key #{encrypted[:key]} and date #{encrypted[:date]}"
+Enigma.encrypt(ARGV[0], ARGV[1])
