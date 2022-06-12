881c4ab2b3033f8a7713c24511028e165f2c78e0
Eli Sachs <easachs13@gmail.com>
2022-06-12 00:30:42 -0600

    Remove shifted array method from Decryptor

--- lib/decryptor.rb
+++ lib/decryptor.rb
@@ -4,21 +4,4 @@ class Decryptor < Cipher
   def decrypt
     { decryption: solution, key: @key, date: @date }
   end
-
-  def shifted_array
-    new_array = []
-    indexed_array.each do |chunk|
-      new_chunk = []; index = 0
-      while index < 4
-        if special_char(chunk[index])
-          new_chunk << chunk[index] unless chunk[index].nil?
-        else
-          new_chunk << (chunk[index] - shifts_array[index]) % 27
-        end
-        index += 1
-      end
-      new_array << new_chunk
-    end
-    new_array
-  end
 end
