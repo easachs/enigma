5b1ef5f1e6522efbe5ce7e562997577ade039e26
Eli Sachs <easachs13@gmail.com>
2022-06-11 18:07:39 -0600

    Update runners for new Writer class

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,4 +1,8 @@
-require_relative 'enigma'
+require_relative 'writer'
 
-Enigma.decrypt(ARGV[0], ARGV[1], ARGV[2], ARGV[3])
+if ARGV.length < 4
+  nil
+else
+  Writer.decrypt(ARGV[0], ARGV[1], ARGV[2], ARGV[3])
+end
 # 38901, 110622
